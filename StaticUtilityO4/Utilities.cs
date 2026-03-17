c7ce48c25e900cbd35573ba79501a9191a606e51
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-16 23:16:19 -0600

    add a semicolon, python glitch still acting up

--- StaticUtilityO4/Utilities.cs
+++ StaticUtilityO4/Utilities.cs
@@ -13,7 +13,7 @@ namespace Utilities
         {
         Console.WriteLine("How are you today");
             string response = Console.ReadLine();
-            Console.WriteLine(response + "? Well... How about that. Isn't that wild?")
+            Console.WriteLine(response + "? Well... How about that. Isn't that wild?");
         //let me know if public on a var is against the rules, 
         // the system didnt seem to like "string response" or 
         // adding public to the start, but then indenting it with 
