ccc7be877075be759fa8f61851616f9e2f4d6e2d
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-16 23:14:45 -0600

    weird python style glitch

--- StaticUtilityO4/Utilities.cs
+++ StaticUtilityO4/Utilities.cs
@@ -1,11 +1,27 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Utilities
 {
     public class UtilitiesClass
     {
         public static void WelcomeMessage()
         {
-        Console.WriteLine("Display welcome message");
+        Console.WriteLine("\nHowdy do! Top of the mornin to ya!");
         }
+        
+        public static void UserResponseToUpper()
+        {
+        Console.WriteLine("How are you today");
+            string response = Console.ReadLine();
+            Console.WriteLine(response + "? Well... How about that. Isn't that wild?")
+        //let me know if public on a var is against the rules, 
+        // the system didnt seem to like "string response" or 
+        // adding public to the start, but then indenting it with 
+        // no public works out just fine?
+
+        //this isnt python why does it LIKE that?! thats weird
+        }
+
         /* //Example Actions
         Display welcome message
 
