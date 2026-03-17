9d8732604c53c239d50f6818da31aa93956ff008
holden.anderson <holden.anderson@students.snow.edu>
2026-03-16 18:23:07 -0600

    quick get some requirements down for namespacing

---
+++ StaticUtilityO4/Utilities.cs
@@ -0,0 +1,32 @@
+namespace Utilities
+{
+    public class Utilitiesclass{
+    static void methodthatmustbestatic()
+    {
+        /* //Example Actions
+        Display welcome message
+
+        Convert text to uppercase
+
+        Count characters in a string
+
+
+        */
+            
+        /* Example Console Interaction
+        (✅Enter text:
+            hello world
+
+        (✅Uppercase: HELLO WORLD
+        (✅Character count: 11
+
+        Restrictions
+        ❌ No instance fields
+
+        ❌ No new
+
+        ❌ No constructors*/
+
+        }
+    }
+}
